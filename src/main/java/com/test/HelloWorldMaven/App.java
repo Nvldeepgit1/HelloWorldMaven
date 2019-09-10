ee5a61f81d4e0d795725c51df9359bd8433790c0
Nvldeepgit1 <54612591+Nvldeepgit1@users.noreply.github.com>
2019-09-10 11:09:44 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello World!" );
          System.out.println( "Welcome to India" );
          System.out.println( "India has 29 states" );
-         System.out.println( "India Launches chandrayan2" );
+         System.out.println( "India Launches chandrayan2  :)" );
          System.out.println( "Salute to ISRO" );
     }
 }
